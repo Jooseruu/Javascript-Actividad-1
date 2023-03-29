fb62ec22c50a6ced72ff7b2824cd57415c3dcb74
Jooseeruu <arjona2908@gmail.com>
2023-03-29 17:46:49 +0200

    Comentamos los 3 metodos utilizados en el .js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let numeros = []; // La función "agregarNumero()" guarda los números en el array "numeros"
 
 function agregarNumero() {
-    let numero = document.getElementById('numero').value;
+    let numero = document.getElementById('numero').value; //getElementById() es un método en JavaScript que se utiliza para seleccionar un elemento HTML que tenga un atributo id específico.
     if (numero !== '') {
-        numeros.push(Number(numero));
+        numeros.push(Number(numero));//El método push() modifica el array original y devuelve la nueva longitud del array.
         document.getElementById('numero').value = '';
     }
 }
 
 function sumarNumeros() { //El botón "Sumar números" ejecuta la función "sumarNumeros()" que suma los números del array
-    let suma = numeros.reduce((a, b) => a + b, 0);
-    document.getElementById('resultado').innerHTML = 'La suma es: ' + suma;
+    let suma = numeros.reduce((a, b) => a + b, 0); //El método reduce() toma dos argumentos: la función reductora y un valor inicial opcional
+    document.getElementById('resultado').innerHTML = 'La suma es: ' + suma; 
 }
